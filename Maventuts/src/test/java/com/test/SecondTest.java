087948b39b48dba29751c355ef6cea00156479aa
Shanu <Shanu@Shanu-PC>
2018-10-17 17:21:28 +0530

    Initial

--- Maventuts/src/test/java/com/test/SecondTest.java
+++ Maventuts/src/test/java/com/test/SecondTest.java
@@ -7,7 +7,6 @@ public class SecondTest {
 	@Test
 	public void tc_3() throws InterruptedException
 	{
-		Thread.sleep(10000);
 		String actualTitle = NewTest.driver.getTitle();
 		System.out.println("Title is  " +   actualTitle);
 	
---
+++ Maventuts/src/test/java/com/testing/SecondTest.java
@@ -0,0 +1,9 @@
+package com.testing;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+  @Test
+  public void f() {
+  }
+}
